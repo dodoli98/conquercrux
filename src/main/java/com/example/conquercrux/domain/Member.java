4ebe4de4c00103ea4154b52d91e2e2cdbfdeafc8
KimSeonGyu <dodoli98@naver.com>
2023-08-06 21:48:49 +0900

    member,product 매퍼 인터페이스, 매퍼 xml

---
+++ src/main/java/com/example/conquercrux/domain/Member.java
@@ -0,0 +1,20 @@
+package com.example.conquercrux.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String member_id;
+
+    private String member_password;
+
+    private String member_name;
+
+    private String member_email;
+
+    private String member_phone_number;
+
+    private String member_address;
+
+}
