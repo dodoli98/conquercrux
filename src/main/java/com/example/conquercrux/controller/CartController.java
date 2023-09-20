3ff1cc100aa8bc4c2c76012a28947416afd0ec94
KimSeonGyu <dodoli98@naver.com>
2023-09-20 20:08:51 +0900

    장바구니 페이지 비동기적 데이터 업데이트 로직

--- src/main/java/com/example/conquercrux/controller/CartController.java
+++ src/main/java/com/example/conquercrux/controller/CartController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -54,8 +56,38 @@ public class CartController {
         } else {
             return "home/home";
         }
-
-
     }
 
+    @GetMapping("/deleteCartItem")
+    public String deleteCartItem(HttpSession session, @RequestParam String product_name, RedirectAttributes redirectAttributes) {
+        String member_id = (String) session.getAttribute("member_id");
+
+        cartService.deleteCartItem(member_id, product_name);
+
+        return "redirect:/cart";
+    }
+
+
+
+    @PostMapping("/updateCart")
+    @ResponseBody // 이 어노테이션을 사용하여 응답 데이터가 JSON 또는 텍스트 형식인 경우를 처리합니다.
+    public String updateCart(@RequestParam("productName") String productName,
+                             @RequestParam("productQuantity") int productQuantity,
+                             HttpSession session) {
+
+        String member_id = (String) session.getAttribute("member_id");
+
+        log.info("product_name = {}", productName);
+        Cart cart = cartService.readCartItem(productName);
+
+        cart.setMember_id(member_id);
+        cart.setProduct_quantity(productQuantity);
+
+        cartService.updateCartItem(cart);
+        // 여기서 productName과 productQuantity를 사용하여 카트 업데이트 로직을 구현합니다.
+        // 예를 들어, 카트 데이터베이스 업데이트 또는 세션에 데이터 저장을 수행할 수 있습니다.
+
+        String jsonResponse = "{\"message\": \"수량이 업데이트되었습니다.\", \"updatedQuantity\": " + productQuantity + "}";
+        return jsonResponse;
+    }
 }
